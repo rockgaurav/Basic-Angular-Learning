ff0d45495086fe8703320b9734fcc40c813c1f1d
Gaurav <gaurav@gmail.com>
2025-05-12 14:04:07 +0530

    All hooks added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LifecycleHooksComponent } from './Components/lifecycle-hooks/lifecycle-hooks.component';
 import { AngularComponent } from './Components/angular/angular.component';
+
+// Lifecycle Hooks imports
+import { LifecycleHooksComponent } from './Components/lifecycle-hooks/lifecycle-hooks.component';
 import { NgOnInitComponent } from './Components/lifecycle-hooks/ng-on-init/ng-on-init.component';
 import { NgOnChangesComponent } from './Components/lifecycle-hooks/ng-on-changes/ng-on-changes.component';
 import { NgDoCheckComponent } from './Components/lifecycle-hooks/ng-do-check/ng-do-check.component';
 import { NgAfterContentInitComponent } from './Components/lifecycle-hooks/ng-after-content-init/ng-after-content-init.component';
+import { NgAfterViewInitComponent } from './Components/lifecycle-hooks/ng-after-view-init/ng-after-view-init.component';
+import { NgAfterViewCheckedComponent } from './Components/lifecycle-hooks/ng-after-view-checked/ng-after-view-checked.component';
+import { NgOnDestroyComponent } from './Components/lifecycle-hooks/ng-on-destroy/ng-on-destroy.component';
 
 const routes: Routes = [
   { path: 'angular', component: AngularComponent },
@@ -14,6 +19,9 @@ const routes: Routes = [
   { path: 'ngOnChanges', component: NgOnChangesComponent },
   { path: 'ngDoCheck', component: NgDoCheckComponent },
   { path: 'ngAfterContentInit', component: NgAfterContentInitComponent },
+  { path: 'ngAfterViewInit', component: NgAfterViewInitComponent },
+  { path: 'ngAfterViewChecked', component: NgAfterViewCheckedComponent },
+  { path: 'ngOnDestroy', component: NgOnDestroyComponent },
 
   // Lazy loading the Lifecycle hooks module
   { path: 'parent', loadChildren: () => import('./modules/lifecycle/parent/parent.module').then(m => m.ParentModule) },
